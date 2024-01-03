1bd071d406fbe2015472e7b44020ac13c2cd9f1e
José Vítor <jose.vitor.oliveira.44@gmail.com>
2024-01-03 14:10:00 -0300

    instructions modal added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { Container } from "./components/Container";
+import GameInstructions from "./components/GameInstructions";
 
 function App() {
   return (
     <main>
+      <GameInstructions />
       <Container />
     </main>
   );
